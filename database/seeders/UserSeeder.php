b0638f5c44dd3b1bf7d9911a9afc13f8c245c814
arlanady <arlanadyma@gmail.com>
2021-03-25 10:05:29 +0700

    praktikum3 custom login username dan password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,8 +15,9 @@ class UserSeeder extends Seeder
     public function run()
     {
     DB::table('users')->insert([
-            'name' => 'Administrator',
-            'email' => 'admin@admin.com',
+            'username' => 'admin',
+            'name' => 'Administrator Baru',
+            'email' => 'adminbaru@admin.com',
             'password' => Hash::make('password'),
         ]);
     }
